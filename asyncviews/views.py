1fa99a351e8704cab6c0b844541f1a62f3faeff9
Kulasq <lucasdantas_11@hotmail.com>
2025-08-14 10:42:38 -0300

    add requirements.txt

--- asyncviews/views.py
+++ asyncviews/views.py
@@ -1,6 +1,4 @@
 import asyncio
-from time import sleep
-
 import httpx
 from django.http import HttpResponse
 
@@ -10,21 +8,11 @@ async def http_call_async():
         await asyncio.sleep(1)
         print(num)
     async with httpx.AsyncClient() as client:
-        r = await client.get("https://httpbin.org/")
+        r = await client.get("https://httpbin.org")
         print(r)
 
-def http_call_sync():
-    for num in range(1, 6):
-        sleep(1)
-        print(num)
-    r = httpx.get("https://httpbin.org/")
-    print(r)
 
 async def async_view(request):
     loop = asyncio.get_event_loop()
     loop.create_task(http_call_async())
     return HttpResponse("Non-blocking HTTP request")
-
-def sync_view(request):
-    http_call_sync()
-    return HttpResponse("Blocking HTTP request")
